de34b918a922ba02d17911bdf36250a84273c520
ApramPepo <120373555+ApramPepo@users.noreply.github.com>
2024-12-07 17:24:44 +0200

    debugging

--- bnw.c
+++ bnw.c
@@ -6,24 +6,52 @@
 
 int main(void)
 {
-    FILE *input = fopen("hyptia", "r");
-    FILE *output = fopen("bw.bmp", "bw");
+    FILE *in = fopen("hypatia.bmp", "r");
+    FILE *out = fopen("bw.bmp", "w+");
 
     int i;
     unsigned char byte[54];
     unsigned char colortable[1024];
 
-    if(input==NULL)
+    if(in==NULL)
     {
-        printf("This file doesn't exist.\n")
+        printf("This file doesn't exist.\n");
     }
 
-    for(i=0; i<54; i++)
+    for(i = 0; i < 54; i++)
     {
-        byte[i]=getc(input);
+        byte[i]=getc(in);
     }
 
-    fwrite(byte,sizeof(unsigned char), 54, output);
+    fwrite(byte,sizeof(unsigned char), 54, out);
 
-    
+    int height = *(int*)&byte[18];
+    int width = *(int*)&byte[22];
+    int bitDepth = *(int*)&byte[28];
+
+    printf("width %d\n", width);
+    printf("height %d\n", height);
+    int size=height*width;
+
+    if(bitDepth <= 8)
+    {
+        fread(colortable, sizeof(unsigned char), 1024, in);
+        fwrite(colortable, sizeof(unsigned char), 1024, out);
+    }
+
+    unsigned char buffer[size];
+
+    fread(buffer, sizeof(unsigned char), size, in);
+
+    for(i = 0; i < size; i++)
+    {
+        buffer[i] = (buffer[i] > dark) ? white : black;
+    }
+
+    fwrite(buffer, sizeof(unsigned char), size, out);
+
+    fclose(in);
+    fclose(out);
+
+    return 0;
 }
